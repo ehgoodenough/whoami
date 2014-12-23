8d886239f8becb48c971d3958315e4d6c08eea4c
Andrew McPherson <andrewmcp333@gmail.com>
2014-12-22 19:25:44 -0500

    Established a method for rendering the style.

--- scripts/Player.js
+++ scripts/Player.js
@@ -1,9 +1,24 @@
 var Player = React.createClass({
+    getInitialState: function() {
+        return {
+            x: 1,
+            y: 1,
+            width: 1,
+            height: 1
+        }
+    },
     render: function() {
         return (
-            <div className="player">
-            </div>
+            <div className="player" style={this.getStyle()}></div>
         )
+    },
+    getStyle: function() {
+        return {
+            top: this.state.x - (this.state.width / 2) + "rem",
+            left: this.state.y - (this.state.height / 2)  + "rem",
+            width: this.state.width + "rem",
+            height: this.state.height + "rem"
+        }
     }
 })
 
