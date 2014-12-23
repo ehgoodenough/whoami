3d5f553649428716d0d7559d2ac1567fcc1e4760
Andrew McPherson <andrewmcp333@gmail.com>
2014-12-22 22:40:24 -0500

    Defined the keyschemes for multiple players.

--- scripts/PlayerConfiguration.js
+++ scripts/PlayerConfiguration.js
@@ -2,6 +2,16 @@ var PlayerConfiguration = {
     "1": {
         "input": {
             "keyboard": {
+                "move up": "up arrow",
+                "move down": "down arrow",
+                "move left": "left arrow",
+                "move right": "right arrow"
+            }
+        }
+    },
+    "2": {
+        "input": {
+            "keyboard": {
                 "move up": "w",
                 "move down": "s",
                 "move left": "a",
@@ -9,16 +19,36 @@ var PlayerConfiguration = {
             }
         }
     },
-    "2": {
+    "3": {
         "input": {
             "keyboard": {
-                "move up": "up arrow",
-                "move down": "down arrow",
-                "move left": "left arrow",
-                "move right": "right arrow"
+                "move up": "i",
+                "move down": "k",
+                "move left": "j",
+                "move right": "l"
             }
         }
-    }
+    },
+    "4": {
+        "input": {
+            "keyboard": {
+                "move up": "numpad 5",
+                "move down": "numpad 2",
+                "move left": "numpad 1",
+                "move right": "numpad 3"
+            }
+        }
+    },
+    "5": {
+        "input": {
+            "keyboard": {
+                "move up": "t",
+                "move down": "g",
+                "move left": "f",
+                "move right": "h"
+            }
+        }
+    },
 }
 
 module.exports = PlayerConfiguration
