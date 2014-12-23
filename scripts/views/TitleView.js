4c356c0d06eb8c167ab9aeac1b9b7e8d27286585
Andrew McPherson <andrewmcp333@gmail.com>
2014-12-23 10:43:03 -0500

    Added a button to jump to the next view.

--- scripts/views/TitleView.js
+++ scripts/views/TitleView.js
@@ -1,11 +1,20 @@
+var CurrentViewActions = require("<root>/scripts/actions/CurrentViewActions")
+var PlayView = require("<root>/scripts/views/PlayView")
+
 var TitleView = React.createClass({
     render: function() {
         return (
             <div id="title" className="view">
                 <h1>iamnpc</h1>
                 <h2>A game about being an NPC</h2>
+                <div className="link" onClick={this.onClick}>
+                    Play!!
+                </div>
             </div>
         )
+    },
+    onClick: function(event) {
+        CurrentViewActions.ChangeView(PlayView)
     }
 })
 
