3145c75b31be35feab5138ad56dacbf3f92cd018
Andrew McPherson <andrewmcp333@gmail.com>
2015-02-01 22:37:45 -0500

    Imported a snippet for reflux from older code.

--- source/index.js
+++ source/index.js
@@ -1,5 +1,8 @@
 window.React = require("react/addons")
 window.Reflux = require("reflux")
 
+window.Reflux.StoreMethods.getInitialState = function() {if(this.getData) {return this.getData()}}
+window.Reflux.StoreMethods.retrigger = function() {if(this.getData) {this.trigger(this.getData())}}
+
 var GameFrame = require("<root>/scripts/components/GameFrame")
 React.render(<GameFrame/>, document.body)
